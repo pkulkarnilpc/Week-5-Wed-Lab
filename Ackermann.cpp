114764156f260a646fd66e79e58d61cf88ca400b
pkulkarnilpc <pkulkarni@laspositascollege.edu>
2025-09-16 20:00:30 -0700

    Add files via upload

---
+++ Ackermann.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Ackermann function definition
+unsigned long long ackermann(unsigned int m, unsigned int n) {
+ // TODO: Complete the code.
+}
+
+int main() {
+    unsigned int m, n;
+    cout << "Enter m and n: ";
+    cin >> m >> n;
+
+    cout << "Ackermann(" << m << ", " << n << ") = " 
+         << ackermann(m, n) << endl;
+
+    return 0;
+}
